9c454dbefe6243e877a99cb776f078ec8451e6c3
ManoelOscar35 <manoeloscar557@gmail.com>
2024-10-31 11:18:31 -0300

    iniciando projeto e layout

---
+++ src/app/features/category/view/category/category.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>
+      <p>Category works</p>
+    </div>
+  `,
+  styles: ``,
+})
+export class CategoryComponent {}
---
+++ src/app/features/task/view/task/task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  template: `<p>Task works!</p>`,
+  styles: ``,
+})
+export class TaskComponent {}
